f7f4b42af502853bd5388ae81bc16e72f52ce419
Crazy Penguin <crazy.frozen.penguin@gmail.com>
2012-07-31 22:53:53 -0400

    updated repo

---
+++ maven-repo/updatelibs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION=1.2.2
+LIBS=(core lang-groovy lang-java lang-jruby lang-jython lang-rhino platform testframework)
+
+for NAME in ${LIBS[*]}
+do
+	printf "Adding vertx-%s-%s.final.jar to local repository...\n" $NAME $VERSION
+	mvn install:install-file -DgroupId=com.vertx -DartifactId=vertx-$NAME -Dversion=$VERSION -Dfile=./vertx-$NAME-$VERSION.final.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=./repository
+done
+
