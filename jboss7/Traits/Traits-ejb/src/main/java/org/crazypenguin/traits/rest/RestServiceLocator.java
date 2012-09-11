1f4c79fbaf687e57fbab7271999660e78640fd33
Crazy Penguin <crazy.frozen.penguin@gmail.com>
2012-09-10 22:39:59 -0400

    Moved Traits to it's own project

--- jboss7/Traits/Traits-ejb/src/main/java/org/crazypenguin/traits/rest/RestServiceLocator.java
+++
@@ -1,9 +0,0 @@
-package org.crazypenguin.traits.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestServiceLocator extends Application {
-
-}
